c874aa38ae6af890e574a111924c569cf3aac2dd
Luis Guerreiro <luiscvlh11@gmail.com>
2023-09-29 16:19:53 -0300

    feat[python]: adicionado push

---
+++ python/main.py
@@ -0,0 +1,33 @@
+class HashTable:
+    vector: list
+    size: int
+
+    def __init__(self, size: int):
+        self.vector = [None for _ in range(size)]
+        self.size = size
+
+    def _hash(self, key: int):
+        # must always return an int between 0 and size -1
+        return key % self.size
+
+    def push(self, key: int):
+        index = self._hash(key)
+        if self.vector[index] is None:
+            self.vector[index] = key
+
+
+if __name__ == "__main__":
+    # hash table with a size of 7
+    hash_table = HashTable(7)
+
+    hash_table.push(15)  # 15 % 7 = 1
+    assert hash_table.vector[1] == 15
+
+    hash_table.push(17)  # 17 % 17 = 3
+    assert hash_table.vector[3] == 17
+
+    hash_table.push(21)  # 21 % 7 = 0
+    assert hash_table.vector[0] == 21
+
+    hash_table.push(7)  # 7 % 7 = 0
+    assert hash_table.vector[0] == 21
