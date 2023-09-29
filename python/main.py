38001471eca3a9e487dd7544032fbf7b273e7e4f
Luis Guerreiro <luiscvlh11@gmail.com>
2023-09-29 17:42:05 -0300

    feat[python]: privatizado atributos

--- python/main.py
+++ python/main.py
@@ -1,37 +1,37 @@
 class HashTable:
-    vector: list
-    vector_size: int
-    quantity_limit_of_items: int
-    quantity_of_inserted_items: int = 0
+    _vector: list
+    _vector_size: int
+    _quantity_limit_of_items: int
+    _quantity_of_inserted_items: int = 0
 
     def __init__(self, vector_size: int):
-        self.vector = [None for _ in range(vector_size)]
-        self.vector_size = vector_size
-        self.quantity_limit_of_items = vector_size // 1.3  # 70%
+        self._vector = [None for _ in range(vector_size)]
+        self._vector_size = vector_size
+        self._quantity_limit_of_items = vector_size // 1.3  # 70%
 
     def _hash(self, key: int):
-        # must always return an int between 0 and vector_size -1
-        return key % self.vector_size
+        # must always return an int between 0 and vector size - 1
+        return key % self._vector_size
 
     def is_full(self):
-        return self.quantity_of_inserted_items == self.quantity_limit_of_items
+        return self._quantity_of_inserted_items == self._quantity_limit_of_items
 
     def push(self, key: int):
         index = self._hash(key)
-        if self.vector[index] is None:
-            self.vector[index] = key
-            self.quantity_of_inserted_items += 1
+        if self._vector[index] is None:
+            self._vector[index] = key
+            self._quantity_of_inserted_items += 1
 
     def search(self, key: int):
         index = self._hash(key)
-        value = self.vector[index]
+        value = self._vector[index]
         return value if key == value else None
 
     def remove(self, key: int):
         index = self._hash(key)
-        if self.vector[index] == key:
-            self.vector[index] = -1  # available
-            self.quantity_of_inserted_items -= 1
+        if self._vector[index] == key:
+            self._vector[index] = -1  # available
+            self._quantity_of_inserted_items -= 1
 
 
 if __name__ == "__main__":
@@ -39,18 +39,18 @@ if __name__ == "__main__":
     hash_table = HashTable(7)
 
     hash_table.push(15)  # 15 % 7 = 1
-    assert hash_table.vector[1] == 15
+    assert hash_table._vector[1] == 15
 
     hash_table.push(17)  # 17 % 17 = 3
-    assert hash_table.vector[3] == 17
+    assert hash_table._vector[3] == 17
 
     hash_table.push(21)  # 21 % 7 = 0
-    assert hash_table.vector[0] == 21
+    assert hash_table._vector[0] == 21
 
     hash_table.push(7)  # 7 % 7 = 0
-    assert hash_table.vector[0] == 21
+    assert hash_table._vector[0] == 21
 
-    assert hash_table.quantity_of_inserted_items == 3
+    assert hash_table._quantity_of_inserted_items == 3
 
     assert not hash_table.is_full()
 
@@ -64,7 +64,7 @@ if __name__ == "__main__":
     assert hash_table.search(7) is None
 
     hash_table.remove(15)
-    assert hash_table.vector[1] == -1
+    assert hash_table._vector[1] == -1
 
     hash_table.remove(7)
-    assert hash_table.vector[0] == 21
+    assert hash_table._vector[0] == 21
